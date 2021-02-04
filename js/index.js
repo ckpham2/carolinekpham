5f7280e59e1fa82f894369e8887ad19396a35876
ckpham2 <64879131+ckpham2@users.noreply.github.com>
2021-02-03 16:14:13 -0800

    Caroline Pham

--- js/index.js
+++ js/index.js
@@ -80,24 +80,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
-$(document).ready(function() {
-    $('.next').on('click',function() {
-        var currentImg= $('.active');
-        var nextImg = currentImg.next();
-
-        if(nextImg.length) {
-            currentImg.removeClass('active').css('z-index', -10);
-            nextImg.addClass('active').css('z-index', 10);
-        }
-    });
-    $('.prec').on('click',function() {
-        var currentImg= $('.active');
-        var prevImg = currentImg.prev();
-
-        if(prevImg.length) {
-            currentImg.removeClass('active').css('z-index', -10);
-            prevImg.addClass('active').css('z-index', 10);
-        }
-    });
-});
